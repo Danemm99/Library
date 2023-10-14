55fc54bf54e41010a37f30283130c793d1e66457
DebugManager <116737219+YuraYarmola@users.noreply.github.com>
2023-10-14 13:45:32 +0300

    123

--- library/order/admin.py
+++ library/order/admin.py
@@ -5,6 +5,6 @@ class AdminOrder(admin.ModelAdmin):
     list_display = ['book', 'user', 'created_at', 'end_at']
     list_filter = ['end_at']
     list_display_links = ['book', 'user']
-    fields = ['book','user', 'plated_end_at','end_at']
+    fields = ['book', 'user', 'plated_end_at','end_at']
 
 admin.site.register(Order, AdminOrder)
