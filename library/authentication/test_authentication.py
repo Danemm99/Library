b826adae930f40c5a068cd4895e88c19dc70f4bf
Danyil Zlobin <alice@programmingmentor.com>
2023-10-20 14:59:57 +0300

    some changes

--- library/authentication/test_authentication.py
+++ library/authentication/test_authentication.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-def test_authentication():git
+def test_authentication():
     driver = webdriver.Edge()
 
     try:
